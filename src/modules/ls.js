627fefddffcc971c4d78d0ea3e2ca5ba470c5d20
Kostia S <kosiak5587@gmail.com>
2024-10-10 10:12:08 +0600

    feat add ls

---
+++ src/modules/ls.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+const getFileType = async (filePath) => {
+  const stats = await fs.stat(filePath);
+  return stats.isDirectory() ? 'directory' : 'file';
+};
+export async function ls(folderPath) {
+  try {
+    const files = await fs.readdir(folderPath);
+    const tableData = await Promise.all(
+      files.map(async (file) => ({
+        Name: file,
+        Type: await getFileType(path.join(folderPath, file)),
+      })),
+    );
+
+    tableData.sort((a, b) => {
+      if (a.Type === 'directory' && b.Type === 'file') return -1;
+      if (a.Type === 'file' && b.Type === 'directory') return 1;
+      return 0;
+    });
+
+    console.table(tableData);
+  } catch (err) {
+    console.error('Error reading directory:', err);
+  }
+}
